c6b08df0529a4831ba67a54fbfae033a61cc49c8
drsmile <drsmile.1001@gmail.com>
2019-05-12 08:40:40 +0800

    加入專案檔案。

---
+++ DivideBySheepSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DivideBySheepSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
